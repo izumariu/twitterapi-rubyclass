719adc924446220c4203bb60ef12644611081cae
sesshomariu <marius.wuerker@t-online.de>
2016-07-31 19:13:25 +0200

    Update date.rb

--- date.rb
+++ date.rb
@@ -10,7 +10,7 @@ def mcc
 	day = sitehtml.match(/<td>\d{1,2}\. bis \d{1,2}\./).to_s.split("<td>")[-1].split(" bis ")[0].split(".")[0].to_i+2
 	year= sitehtml.match(/<td>\d{1,2}\. bis \d{1,2}\. M\&auml\;rz \d{4}/).to_s.split(" ")[-1]
 	$days_left = (Date.parse("#{day}.03.#{year}")-Date.today).to_i
-	puts $days_left
+	#puts $days_left
 end
 def calcToDate(date);$days_left = (Date.parse(date)-Date.today);end
 # DOES:
